8ed4dc6dbd8ca3da6ff3ad4b674a95a34528edb8
Arthur Perboni <arthurperboni@hotmail.com>
2022-05-04 19:55:58 +0000

    contacts + smooth scroll

--- main.js
+++ main.js
@@ -38,3 +38,17 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 })
+
+//Scroll Reveal (Mostrar Elementos ao usar o Scroll)
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  '#home .image, #home .text, #about .image, #about .text, #services .header, #services .cards, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links',
+  { interval: 100 }
+)
